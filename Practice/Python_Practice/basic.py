95ca2cc434fd314bee43dca2617165d6b4bba565
Anmol Srivastava <anmolsrivastava678@gmail.com>
2026-07-21 13:27:50 +0000

    Practice Session

---
+++ Practice/Python_Practice/basic.py
@@ -0,0 +1,30 @@
+# Basic Arithmetic Operation
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+print("Value of First Number:", a)
+print("Value of Second Number:", b)
+
+print("\n----- Basic Calculations -----")
+
+# Addition
+print("Addition:", a + b)
+
+# Subtraction
+print("Subtraction:", a - b)
+
+# Multiplication
+print("Multiplication:", a * b)
+
+# Division
+print("Division:", a / b)
+
+# Floor Division
+print("Floor Division:", a // b)
+
+# Modulus (Remainder)
+print("Modulus:", a % b)
+
+# Exponent (Power)
+print("Power:", a ** b)
