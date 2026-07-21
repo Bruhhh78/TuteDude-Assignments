3833e1fb05e88aa36143715b92ec0d369d8ef68c
Anmol Srivastava <anmolsrivastava678@gmail.com>
2026-07-21 12:43:33 +0000

    Python Program With Datatypes

---
+++ Practice/Python_Practice/first.py
@@ -0,0 +1,21 @@
+# Printing values with their data types
+
+name = "Anmol"
+age = 23
+height = 6.1
+is_student = True
+marks = [85, 90, 95]
+details = ("Python", "Programming")
+student = {"name": "Anmol", "age": 22}
+unique_numbers = {1, 2, 3, 4}
+nothing = None
+
+print("Value:", name, " | Data Type:", type(name))
+print("Value:", age, " | Data Type:", type(age))
+print("Value:", height, " | Data Type:", type(height))
+print("Value:", is_student, " | Data Type:", type(is_student))
+print("Value:", marks, " | Data Type:", type(marks))
+print("Value:", details, " | Data Type:", type(details))
+print("Value:", student, " | Data Type:", type(student))
+print("Value:", unique_numbers, " | Data Type:", type(unique_numbers))
+print("Value:", nothing, " | Data Type:", type(nothing))
